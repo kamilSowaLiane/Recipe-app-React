d5ceeea64dae88558a706666e15ab3faf220ab9b
kamilSowaLiane <kamil06076@gmail.com>
2020-03-20 19:03:20 +0100

    CSS - Result page

--- src/components/Form.js
+++ src/components/Form.js
@@ -21,61 +21,67 @@ const Form = ({ getRecipes }) => {
                 <input type='text' name='recipeName'/>
             </label>
             <div className='filter'>
-                <label>
-                    Hits
-                    <input type='text' name='hits'/>
-                </label>
-                <label>
-                    Calories
-                    <input type='text' name='calories'/>
-                </label>
-                <label>
-                    Maximum number of ingredients
-                    <input type='text' name='ingr'/>
-                </label>
-                <label>
-                    excluded
-                    <input type='text' name='excluded'/>
-                </label>
-                <label>
-                    Diet
-                    <select name="diet" defaultValue=''>
-                        <option value="">None</option>
-                        <option value="balanced">Balanced</option>
-                        <option value="high-protein">High protein</option>
-                        <option value="high-fiber">High fiber</option>
-                        <option value="low-fat">Low fat</option>
-                        <option value="low-carb">Low carb</option>
-                        <option value="low-sodium">Low sodium</option>
-                    </select>
-                </label>
-                <label>
-                    <input type="checkbox" value='alcohol-free' name='health'/>
-                    Alcohol-free
-                </label>
-                <label>
-                    <input type="checkbox" value='peanut-free' name='health'/>
-                    Peanut-free
-                </label>
-                <label>
-                    <input type="checkbox" value='tree-nut-free' name='health'/>
-                    Tree nuts free
-                </label>
-                <label>
-                    <input type="checkbox" value='sugar-conscious' name='health'/>
-                    Sugar conscious
-                </label>
-                <label>
-                    <input type="checkbox" value='vegetarian' name='health'/>
+                <div className='input-text'>
+                    <label>
+                        Hits
+                        <input type='text' name='hits'/>
+                    </label>
+                    <label>
+                        Calories
+                        <input type='text' name='calories'/>
+                    </label>
+                    <label>
+                        Maximum number of ingredients
+                        <input type='text' name='ingr'/>
+                    </label>
+                    <label>
+                        excluded
+                        <input type='text' name='excluded'/>
+                    </label>
+                    <label>
+                        Diet
+                        <select name="diet" defaultValue=''>
+                            <option value="">None</option>
+                            <option value="balanced">Balanced</option>
+                            <option value="high-protein">High protein</option>
+                            <option value="high-fiber">High fiber</option>
+                            <option value="low-fat">Low fat</option>
+                            <option value="low-carb">Low carb</option>
+                            <option value="low-sodium">Low sodium</option>
+                        </select>
+                    </label>
+                </div>
+                <div className='input-checkbox'>
+                    <label>
+                        <input type="checkbox" value='alcohol-free' name='health'/>
+                        Alcohol-free
+                    </label>
+                    <label>
+                        <input type="checkbox" value='peanut-free' name='health'/>
+                        Peanut-free
+                    </label>
+                    <label>
+                        <input type="checkbox" value='tree-nut-free' name='health'/>
+                        Tree nuts free
+                    </label>
+                    <label>
+                        <input type="checkbox" value='sugar-conscious' name='health'/>
+                        Sugar conscious
+                    </label>
+                    <label>
+                        <input type="checkbox" value='vegetarian' name='health'/>
                     Vegetarian
-                </label>
-                <label>
-                    <input type="checkbox" value='vegan' name='health'/>
-                    Vegan
-                </label>
+                    </label>
+                    <label>
+                        <input type="checkbox" value='vegan' name='health'/>
+                        Vegan
+                    </label>
+                </div>
             </div>
-            <button type='button' onClick={handleClick}>{isActive ? 'Hide filter' : 'Show filter'}<i className="icon-filter" /></button>
-            <button>Search<i className="icon-search" /></button>
+            <div className='buttons'>
+                <button type='button' onClick={handleClick}>{isActive ? 'Hide filter' : 'Show filter'}<i className="icon-filter" /></button>
+                <button>Search<i className="icon-search" /></button>
+            </div>
         </form>
     )
 }
--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -4,24 +4,31 @@ import { Link } from 'react-router-dom'
 const Recipe = ({recipes}) => {
     const recipeList = recipes.map(recipe => {
         return (
-            <div key={recipe.recipe.uri}>
-                <h1>{recipe.recipe.label}</h1>
-                <p>{Math.floor(recipe.recipe.calories)} calories</p>
-                <p>Total meal weight {Math.floor(recipe.recipe.totalWeight)} g</p>
-                <p>Servings: {recipe.recipe.yield}</p>
+            <div className='recipe' key={recipe.recipe.uri}>
+                <h2>
+                    {recipe.recipe.label.length < 21 ? `${recipe.recipe.label}` : `${recipe.recipe.label.substring(0, 21)}...`}
+                </h2>
+                <p>by {recipe.recipe.source}</p>
                 <img src={recipe.recipe.image} alt=""></img>
                 <button>
                     <Link to={{ 
                         pathname: `/ingredients/${recipe.recipe.uri.substring(51, 83)}`,
                         state: { recipe: recipe.recipe.uri.substring(51, 83) }
-                    }}>Ingredients</Link>
+                    }}>View ingredients & more</Link>
                 </button>
             </div>
         )
     })
+const result = recipeList.length > 1 ? recipeList : 
+    <div className='info'>
+        <p>Nothing found yet.</p>
+        <p>Start searching for recipes by typing in any ingredient name e.g. cheese, chicken or meal name e.g. spaghetti. 
+            Use filters to specify your searching. Have a good meal!
+        </p>
+    </div>
     return (
         <div className='results'>
-            {recipeList}
+            {result}
         </div>
     )
 }
